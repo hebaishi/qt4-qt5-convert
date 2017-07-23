cb38eff42d8c7615df2cf48d673170ddc659e546
hebaishi <hebaishi.husam@gmail.com>
2017-07-23 23:13:54 +0100

    Improve method printer

--- src/CustomPrinter.cpp
+++ src/CustomPrinter.cpp
@@ -9,17 +9,25 @@ void printMethod(const clang::CXXMethodDecl *declaration)
 {
     auto methodName = declaration->getAsFunction()->getNameInfo().getAsString();
     auto className = declaration->getParent()->getNameAsString();
+    auto returnType = declaration->getReturnType().getAsString();
     auto& Context = declaration->getASTContext();
     auto fullLocation = Context.getFullLoc(declaration->getLocStart());
-    llvm::errs() << fullLocation.getFileEntry()->getName();
-    llvm::errs() << ":" << fullLocation.getSpellingLineNumber();
-    llvm::errs() << ":" << fullLocation.getSpellingColumnNumber() << " ";
-    llvm::errs() << "Found " << className << "::" << methodName << "( ";
-    llvm::errs() << declaration->getParamDecl(0)->getType().getAsString();
-    for (auto i=1ul ; i < declaration->getNumParams() ; i++)
+
+    llvm::errs()
+        << fullLocation.getFileEntry()->getName()
+        << ":" << fullLocation.getSpellingLineNumber()
+        << ":" << fullLocation.getSpellingColumnNumber() << ": "
+        << "Found " << returnType << " "
+        << className << "::" << methodName << " (";
+    if (declaration->getNumParams() > 0)
     {
-        llvm::errs() << ", " << declaration->getParamDecl(i)->getType().getAsString();
+        llvm::errs() << declaration->getParamDecl(0)->getType().getAsString();
+        for (auto i=1ul ; i < declaration->getNumParams() ; i++)
+        {
+            llvm::errs() << ", " << declaration->getParamDecl(i)->getType().getAsString();
+        }
     }
+
     llvm::errs() << ")\n";
 }
 
